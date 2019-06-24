598f48e0e4b80a35ebfdaf9d8cae5f7151922199
AndrewBeznosko <andrew.beznosko@gmail.com>
2019-06-24 16:46:58 +0300

    steps page, ready 1-st step PC

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    var form = $("#steps_form");
+    form.validate({
+        errorPlacement: function errorPlacement(error, element) {
+            element.before(error);
+        },
+        rules: {
+            confirm: {
+                equalTo: "#password"
+            }
+        }
+    });
+    form.children("div").steps({
+        headerTag: "h3",
+        titleTemplate: "<span class='number'>#index#</span>",
+        bodyTag: "section",
+        showBackButton: false,
+        enablePagination: false,
+        saveState: true,
+        // startIndex: 1,
+        onStepChanging: function(event, currentIndex, newIndex) {
+            form.validate().settings.ignore = ":disabled,:hidden";
+            return form.valid();
+        },
+        onFinishing: function(event, currentIndex) {
+            form.validate().settings.ignore = ":disabled";
+            return form.valid();
+        },
+        onFinished: function(event, currentIndex) {
+            alert("Submitted!");
+        }
+    });
+
+    $(document).delegate('.js-next-step-btn', 'click', function() {
+        form.children("div").steps("next");
+    });
+});
