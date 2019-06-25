de3a286746bff2c9084e55f3f016370bc705016e
AndrewBeznosko <andrew.beznosko@gmail.com>
2019-06-25 14:31:22 +0300

    step 4 ready PC

--- js/script.js
+++ js/script.js
@@ -17,15 +17,15 @@ $(document).ready(function() {
         showBackButton: false,
         enablePagination: false,
         saveState: true,
-        startIndex: 2,
+        startIndex: 3,
         onStepChanging: function(event, currentIndex, newIndex) {
             form.validate().settings.ignore = ":disabled,:hidden";
             return form.valid();
             console.log('ну привет');
             if (form.children("div").steps("getCurrentIndex") == 0) {
-                $("body").css("background-color","#fff");
+                $("body").css("background-color", "#fff");
             } else {
-                $("body").css("background-color","#F4F4F4");
+                $("body").css("background-color", "#F4F4F4");
             }
         },
         onFinishing: function(event, currentIndex) {
@@ -40,4 +40,43 @@ $(document).ready(function() {
     $(document).delegate('.js-next-step-btn', 'click', function() {
         form.children("div").steps("next");
     });
+
+
+
+    // js_checklist
+    var goalList = $('.js_checklist');
+    var countGoalList = $('.js_checklist li').length;
+    var elems = $('.js_checklist li');
+    console.log(countGoalList);
+    $(".js_add_goal").on("click", function() {
+        countGoalList++;
+        goalList.append("<li>" +
+            "<label>" +
+            "Цель №<span>1</span>" +
+            "<input type='text' placeholder='Введите сюда свою цель''>" +
+            "<span class='delete_btn js_delete_btn'></span>" +
+            "</label>" +
+            "</li>"
+        );
+        iconRemove();
+        NumberingList();
+    });
+    iconRemove();
+
+    function iconRemove() {
+        $(".js_delete_btn").click(function() {
+            $(this).closest('li').remove();
+
+            NumberingList();
+        });
+    }
+
+    function NumberingList() {
+        $(goalList).each(function() {
+            $('li', this).each(function(i) {
+                $(this).find('label span:first-of-type').text(i + 1);
+            })
+        })
+    }
+
 });
